08e7f97bfbf665b7c595fd8c0c3ff548045302db
Sushrut Warekar <sware8@illinois.edu>
2024-08-11 11:03:59 -0500

    working on ui, shadow needs work

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 'use client';
 import { Box, Stack, TextField, Button, Typography } from "@mui/material";
+import zIndex from "@mui/material/styles/zIndex";
 import { useState, useRef, useEffect } from "react";
 
 export default function Home() {
@@ -60,108 +61,129 @@ export default function Home() {
   };
 
   return (
-    <main> 
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-      <h1 className="text-4xl p-4 text-center font-sans">uiucGPT</h1>
+    <main>
+      <div
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+        width: '100%',
+        fontFamily: 'monospace',
+        fontSize: '0.875rem', // This is equivalent to text-sm in Tailwind CSS
+        // backgroundColor: 'blue'
+      }}
+      >
+        <h1
+        style={{
+          fontSize: '2.25rem', // This is equivalent to text-4xl in Tailwind CSS
+          padding: '1rem 0 0 0', // This is equivalent to p-4 in Tailwind CSS
+          textAlign: 'center',
+          fontFamily: 'sans-serif'
+        }}
+        >uiucGPT
+        </h1>
       </div>
 
       <Box
-      width={'100vw'}
-      height={'100vh'}
+      width={'100%'}
+      height={'90vh'}
       display={'flex'}
       alignItems={'center'}
       justifyContent={'center'}
-    >
-      
-      <Stack 
-        direction={'column'} 
-        justifyContent={'flex-end'}
-        width={'50%'} 
-        height={'80%'} 
-        maxHeight={'80%'} 
-        border={'2px solid black'} 
-        borderRadius={5}
-        spacing={3}
-        overflow='hidden'
       >
-        <Stack direction={'column'} spacing={2} overflow={'auto'} mb={2}
-        sx={{
-          scrollbarWidth: 'thin',
-          scrollbarColor: ' #5E6669 #E8E9EA'
-        }}
+      
+        <Stack 
+          direction={'column'} 
+          justifyContent={'flex-end'}
+          width={'50%'} 
+          height={'80%'} 
+          maxHeight={'80%'} 
+          border={'2px solid black'} 
+          borderRadius={5}
+          spacing={3}
+          overflow='hidden'
         >
-          {/* Initial message from the assistant */}
-          <Box
-            display={'flex'}
-            justifyContent={'flex-start'} // Align the initial message to the right
-            px={1}
+          <Stack direction={'column'} spacing={2} overflow={'auto'} mb={2}
+          sx={{
+            scrollbarWidth: 'thin',
+            scrollbarColor: ' #5E6669 #E8E9EA'
+          }}
           >
+            {/* Initial message from the assistant */}
             <Box
-              bgcolor={'#1F4096'}
-              borderRadius={5}
-              p={2}
-              mt={2}
-              maxWidth={'60%'} // Limit the width of the text bubble
-            >
-              <Typography color={'white'}>
-                {firstMessage}
-              </Typography>
-            </Box>
-          </Box>
-
-          {/* Render the chat history */}
-          {history.map((textObject, index) => (
-            <Box
-              key={index}
               display={'flex'}
-              justifyContent={textObject.role === 'user' ? 'flex-end' : 'flex-start'}
-              paddingX={1}
-              ref={scrollRef}
-              margin={5}
+              justifyContent={'flex-start'} // Align the initial message to the right
+              px={1}
             >
               <Box
-                bgcolor={textObject.role === 'user' ? '#ff5f05' : '#1F4096'}
-                color={'white'}
+                bgcolor={'#1F4096'}
                 borderRadius={5}
                 p={2}
+                mt={2}
                 maxWidth={'60%'} // Limit the width of the text bubble
               >
-                {/* Render HTML content */}
-                <Typography
-                  dangerouslySetInnerHTML={{ __html: textObject.parts[0].text }}
-                />
+                <Typography color={'white'}>
+                  {firstMessage}
+                </Typography>
               </Box>
             </Box>
-          ))}
-        </Stack>
 
-        {/* Input field and send button */}
-        <Stack direction={'row'} spacing={2} width={'98%'} display={'flex'} alignItems={'center'} justifyContent={'space-between'} pb={1} pl='4px'>
+            {/* Render the chat history */}
+            {history.map((textObject, index) => (
+              <Box
+                key={index}
+                display={'flex'}
+                justifyContent={textObject.role === 'user' ? 'flex-end' : 'flex-start'}
+                paddingX={1}
+                ref={scrollRef}
+                margin={5}
+              >
+                <Box
+                  bgcolor={textObject.role === 'user' ? '#ff5f05' : '#1F4096'}
+                  color={'white'}
+                  borderRadius={5}
+                  p={2}
+                  maxWidth={'60%'} // Limit the width of the text bubble
+                  boxShadow={textObject.role === 'user' ? '3px 5px 5px #ff8540' : '-3px 5px 5px #3c59a3'}
+                >
+                  {/* Render HTML content */}
+                  <Typography
+                    dangerouslySetInnerHTML={{ __html: textObject.parts[0].text }}
+                  />
+                </Box>
+              </Box>
+            ))}
+          </Stack>
+
+          {/* Input field and send button */}
+          <Stack direction={'row'} spacing={2} width={'98%'} display={'flex'} alignItems={'center'} justifyContent={'space-between'} pb={1} pl='4px' marginTop='4px'>
+            
+          <TextField 
           
-        <TextField 
-        
-            /* label='Message'*/
-            value={message}
-            placeholder="Message"
-            onChange={(e => setMessage(e.target.value))}
-            fullWidth
-            sx={{ paddingLeft: '5px'}}
-            onKeyDown={(k) => {
-              if (k.key === 'Enter'){
-                sendMessage();
-              }
+              /* label='Message'*/
+              value={message}
+              placeholder="Message"
+              onChange={(e => setMessage(e.target.value))}
+              fullWidth
+              pt={0}
+              mt={0}
+              sx={{ paddingLeft: '5px'}}
+              onKeyDown={(k) => {
+                if (k.key === 'Enter'){
+                  sendMessage();
+                }
+              }}
+            />
+            <Button variant='contained'
+            onClick={sendMessage}
+            style={{
+              minWidth: '75px',
+              minHeight: '45px'
             }}
-          />
-          <Button variant='contained'
-          onClick={sendMessage}
-          style={{
-            minWidth: '75px',
-            minHeight: '45px'
-          }}
-          >Send</Button>
+            >Send</Button>
+          </Stack>
         </Stack>
-      </Stack>
-    </Box>
+      </Box>
     </main>
     
   );
